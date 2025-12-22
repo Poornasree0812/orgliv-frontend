42bfd92c4536d847868f8394c14365bf6e666cb2
Poornasree <poornasree0812@gmail.com>
2025-12-22 22:33:46 +0530

    use production backend url

--- src/api.js
+++ src/api.js
@@ -1,40 +1,13 @@
-// src/api.js
 import axios from "axios";
 
-// Base URL (backend)
 const API = axios.create({
-    baseURL: "https://orgliv-backend.onrender.com/api",
+  baseURL: "https://orgliv-backend.onrender.com/api",
 });
 
-// ----------------------------------
-// REQUEST INTERCEPTOR
-// ----------------------------------
-API.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
-
-// ----------------------------------
-// RESPONSE INTERCEPTOR (OPTIONAL)
-// ----------------------------------
-API.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    // Auto logout if token expired
-    if (error.response && error.response.status === 401) {
-      localStorage.removeItem("token");
-      localStorage.removeItem("user");
-    }
-    return Promise.reject(error);
-  }
-);
+API.interceptors.request.use((req) => {
+  const token = localStorage.getItem("token");
+  if (token) req.headers.Authorization = `Bearer ${token}`;
+  return req;
+});
 
 export default API;
